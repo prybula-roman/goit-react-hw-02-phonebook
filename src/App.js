49df836b875275cf5160bde9ae157972fe06007a
prybula_roman <roman.bismail@gmail.com>
2022-02-23 16:56:19 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Form from "./components/form/form";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
   render() {
+    const newId = nanoid();
+    console.log("newId=", newId);
     return (
       <>
         <Form />
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,9 +2,21 @@ import React from "react";
 
 class Form extends React.Component {
   satete = {};
-
   render() {
-    return <>test</>;
+    return (
+      <div>
+        <h2>Name</h2>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+
+        <button type="submit">Add contact</button>
+      </div>
+    );
   }
 }
 export default Form;
